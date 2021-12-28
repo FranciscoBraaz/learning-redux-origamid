731960d1b6eec643dd676d81a51617f7ffdb83c0
FranciscoBraaz <francisco_braaz@hotmail.com>
2021-12-28 14:11:31 -0300

    createAsyncSlice

--- react-com-redux/react-app/src/store/configureStore.js
+++ react-com-redux/react-app/src/store/configureStore.js
@@ -1,5 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 // import { logger } from '../middleware/logger';
 import contador from './contador';
 import modal from './modal';
--- react-com-redux/react-app/src/store/login.js
+++ react-com-redux/react-app/src/store/login.js
@@ -1,93 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+import createAsyncSlice from './createAsyncSlice';
 
-const slice = createSlice({
-  name: 'login',
-  initialState: {
-    token: {
-      loading: false,
-      data: null,
-      error: null,
+const token = createAsyncSlice({
+  name: 'token',
+  fetchConfig: (payload) => ({
+    url: 'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
     },
-    user: {
-      loading: false,
-      data: null,
-      error: null,
-    },
-  },
-  reducers: {
-    fetchTokenStarted(state) {
-      state.token.loading = true;
-    },
-    fetchTokenSuccess(state, action) {
-      state.token.loading = false;
-      state.token.data = action.payload;
-      state.token.error = null;
-    },
-    fetchTokenError(state, action) {
-      state.token.loading = false;
-      state.token.data = null;
-      state.token.error = action.payload;
-    },
-    fetchUserStarted(state) {
-      state.user.loading = true;
-    },
-    fetchUserSuccess(state, action) {
-      state.user.loading = false;
-      state.user.data = action.payload;
-      state.user.error = null;
-    },
-    fetchUserError(state, action) {
-      state.user.loading = false;
-      state.user.data = null;
-      state.user.error = action.payload;
-    },
-  },
+  }),
 });
 
-const {
-  fetchTokenError,
-  fetchTokenStarted,
-  fetchTokenSuccess,
-  fetchUserError,
-  fetchUserStarted,
-  fetchUserSuccess,
-} = slice.actions;
-
-const fetchToken = (user) => async (dispatch) => {
-  try {
-    dispatch(fetchTokenStarted());
-    const response = await fetch(
-      'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      },
-    );
-    const data = await response.json();
-    return dispatch(fetchTokenSuccess(data));
-  } catch (error) {
-    return dispatch(fetchTokenError(error.message));
-  }
-};
-
-const fetchUser = (token) => async (dispatch) => {
-  try {
-    dispatch(fetchUserStarted());
-    const response = await fetch('https://dogsapi.origamid.dev/json/api/user', {
+const user = createAsyncSlice({
+  name: 'user',
+  fetchConfig: (payload) => ({
+    url: 'https://dogsapi.origamid.dev/json/api/user',
+    options: {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: 'Bearer ' + payload,
       },
-    });
-    const data = await response.json();
-    return dispatch(fetchUserSuccess(data));
-  } catch (error) {
-    return dispatch(fetchUserError(error.message));
-  }
-};
+    },
+  }),
+});
+
+const fetchToken = token.asyncAction;
+const fetchUser = user.asyncAction;
+const tokenReducer = token.reducer;
+const userReducer = user.reducer;
+
+const reducer = combineReducers({ token: tokenReducer, user: userReducer });
+export default reducer;
 
 export const fetchLogin = (user) => async (dispatch) => {
   try {
@@ -97,5 +44,3 @@ export const fetchLogin = (user) => async (dispatch) => {
     }
   } catch (error) {}
 };
-
-export default slice.reducer;
